b2f4f0ae673252c28b4b1f6dec80b1ca8366207e
Ilesha23 <ilyayakovlev0123@gmail.com>
2021-09-04 14:17:10 +0300

    Labs

---
+++ Labs/!10/!10А/!10А.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include "Figure.cpp"
+#include "Area.cpp"
+
+using namespace std;
+
+int main()
+{
+	/*Figure square(3.0);
+	Figure rectangle(3.0, 5.0);
+
+	cout << square.area() << endl;
+	cout << rectangle.area() << endl;*/
+
+	Area fig1;
+	Area fig2;
+
+	cout << fig1.area(2.0) << endl;
+	cout << fig2.area(2.0, 8.0) << endl;
+}
