9b7f90f9345f0ce763995c77e0d09c4d45e1b388
Ilesha23 <ilyayakovlev0123@gmail.com>
2021-09-10 00:27:43 +0300

    Lab !11 added lab !11Д

--- Labs/!11/!11Д/Mult.h
+++ Labs/!11/!11Д/Mult.h
@@ -1,15 +1,5 @@
-//–азработать класс дл€ представлени€ объекта множество чисел(элементы не повтор€ютс€).
-//Ц ќпределить конструктор с трем€ параметрами(количество элементов, минимальный и максимальный
-//    в диапазоне генерируемых чисел), который задаЄт элементы множества(конструктор с параметрами).
-//    Ц ќпределить конструктор, который генерирует множество из 10 элементов в диапазоне[-20; 20]
-//    (конструктор по умолчанию).
-//    Ц ќпределить конструктор, которые копирует в новое множество элементы другого множества, не
-//    превышающие заданное значение(конструктор копировани€).
-//    Ц —оздать функцию, котора€ находит максимальное значение в множестве.
-//    Ц —оздать функции дл€ построени€ объединени€, пересечени€, разности и дизъюнктивной суммы двух
-//    числовых множеств.
-
 #include <random>
+#include <iomanip>
 using namespace std;
 
 class Mult
@@ -19,13 +9,60 @@ private:
 	int size;
 
 public:
-	Mult(int size, int x, int y) {
+	Mult() {
 		srand(time(NULL));
-		setSize(size);
+		setSize(10);
 		mas = new int[getSize()];
 		for (int i = 0; i < getSize(); i++)
 		{
-			mas[i] = rand() % x - y;
+			mas[i] = -20 + rand() % (20 + 20);
+		}
+	}
+	Mult(int size, int x, int y) {
+		if ((abs(x) + abs(y)) >= size)
+		{
+			srand(time(NULL));
+			setSize(size);
+			mas = new int[getSize()];
+			bool b = true;
+			int a, q = 0;
+			do
+			{
+				a = x + rand() % (y - x + 1);
+				for (int i = 0; i < q; i++)
+				{
+					if (a == mas[i])
+					{
+						b = false;
+						break;
+					}
+					b = true;
+				}
+				if (b)
+				{
+					mas[q] = a;
+					q++;
+				}
+			} while (q < getSize());
+		}
+		else
+		{
+			cout << "too small range, created default massive" << endl;
+			srand(time(NULL));
+			setSize(10);
+			mas = new int[getSize()];
+			for (int i = 0; i < getSize(); i++)
+			{
+				mas[i] = -20 + rand() % (20 + 20);
+			}
+		}
+	}
+	Mult(const Mult &mult) {
+		Mult::setSize(mult.size);
+		Mult::mas = new int[getSize()];
+		for (int i = 0; i < getSize(); i++)
+		{
+			Mult::mas[i] = mult.mas[i];
 		}
 	}
 	void setSize(int size) {
@@ -37,8 +74,97 @@ public:
 	void print() {
 		for (int i = 0; i < getSize(); i++)
 		{
-			cout << mas[i] << " ";
+			cout << setw(3) << mas[i];
+		}
+		cout << endl;
+	}
+	int findMax() {
+		cout << setw(13) << "Max number: ";
+		int buf = mas[0];
+		for (int i = 0; i < getSize(); i++)
+		{
+			if (buf < mas[i])
+			{
+				buf = mas[i];
+			}
+		}
+		cout << setw(3) << buf << endl;
+		return buf;
+	}
+	void ob(Mult a, Mult b) {
+		cout << setw(13) << "Merge: ";
+		bool bb = false;
+		for (int i = 0; i < a.getSize(); i++)
+		{
+			cout << setw(3) << a.mas[i];
+		}
+		for (int i = 0; i < b.getSize(); i++)
+		{
+			int q = b.mas[i];
+			for (int j = 0; j < a.getSize(); j++)
+			{
+				if (q == a.mas[j])
+				{
+					bb = false;
+					break;
+				}
+				bb = true;
+			}
+			if (bb)
+			{
+				cout << setw(3) << b.mas[i];
+			}
+		}
+		cout << endl;
+	}
+	void per(Mult a, Mult b) {
+		cout << setw(13) << "Crossing: ";
+		bool bb = false;
+		for (int i = 0; i < b.getSize(); i++)
+		{
+			for (int j = 0; j < a.getSize(); j++)
+			{
+				if (b.mas[i] != a.mas[j])
+				{
+					bb = false;
+				}
+				else {
+					bb = true;
+					break;
+				}
+			}
+			if (bb)
+			{
+				cout << setw(3) << b.mas[i];
+			}
+		}
+		cout << endl;
+	}
+	void razn(Mult a, Mult b) {
+		bool bb = false;
+		for (int i = 0; i < a.getSize(); i++)
+		{
+			for (int j = 0; j < b.getSize(); j++)
+			{
+				if (a.mas[i] == b.mas[j])
+				{
+					bb = false;
+					break;
+				}
+				else
+				{
+					bb = true;
+				}
+			}
+			if (bb)
+			{
+				cout << setw(3) << a.mas[i];
+			}
 		}
 	}
+	void dizz(Mult a, Mult b) {
+		cout << setw(13) << "Disjunction: ";
+		Mult::razn(a, b);
+		Mult::razn(b, a);
+	}
 };
-
