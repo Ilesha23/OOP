a7b73536ebca331661db46cb5c8bea62c1be09c8
Ilesha23 <ilyayakovlev0123@gmail.com>
2021-09-11 22:46:48 +0300

    Finished lab !11E

--- Labs/!11/!11Е/!11Е.cpp
+++ Labs/!11/!11Е/!11Е.cpp
@@ -1,9 +1,66 @@
 ﻿#include <iostream>
+#include <iomanip>
 #include "Time.h"
 using namespace std;
 
 int main()
 {
-	Time a(1, 2, 3);
-	a.print();
+    Time t1, t2;
+    t1.print();
+    t2.print();
+    int a, c;
+    do
+    {
+        cout << endl;
+        cout << setw(22) << "Set first time - 1" << endl; cout << setw(22) << "Set second time - 2" << endl; cout << setw(22) << "Add to first time - 3" << endl; cout << setw(22) << "Add to second time - 4" << endl; cout << setw(22) << "Difference - 5" << endl; cout << setw(22) << "Degree - 6" << endl; cout << setw(21) << "  Enter: ";
+        cin >> a;
+        switch (a)
+        {
+        case 1:
+            system("cls"); t1.print(); t2.print(); cout << endl;
+            t1.setTime();
+            system("cls"); t1.print(); t2.print(); cout << endl;
+            break;
+        case 2:
+            system("cls"); t1.print(); t2.print(); cout << endl;
+            t2.setTime();
+            system("cls"); t1.print(); t2.print(); cout << endl;
+            break;
+        case 3:
+            system("cls"); t1.print(); t2.print(); cout << endl;
+            int x, y, z;
+            cout << "Enter hours, mins and seconds: ";
+            cin >> x >> y >> z;
+            t1.add(x, y, z);
+            system("cls"); t1.print(); t2.print(); cout << endl;
+            break;
+        case 4:
+            system("cls"); t1.print(); t2.print(); cout << endl;
+            cout << "Enter hours, mins and seconds: ";
+            cin >> x >> y >> z;
+            t2.add(x, y, z);
+            system("cls"); t1.print(); t2.print(); cout << endl;
+            break;
+        case 5:
+            system("cls"); t1.print(); t2.print(); cout << endl;
+            t1.compare(t1, t2);
+            break;
+        case 6:
+            system("cls"); t1.print(); t2.print(); cout << endl;
+            cout << "First time - 1" << endl << "Second time - 2" << endl << "Enter: "; cin >> c; system("cls");
+            if (c == 1)
+            {
+                t1.print(); t2.print(); cout << endl;
+                t1.deg();
+            }
+            else
+            {
+                t1.print(); t2.print(); cout << endl;
+                t2.deg();
+            }
+            break;
+        default:
+            break;
+        }
+    } while (a <= 6);
 }
--- Labs/!11/!11Е/Time.h
+++ Labs/!11/!11Е/Time.h
@@ -1,6 +1,9 @@
+#include <iostream>
+using namespace std;
+
 class Time {
 private:
-	int hour, min, sec;
+	int hour = 0, min = 0, sec = 0;
 
 public:
 	Time() {
@@ -13,26 +16,203 @@ public:
 		setM(m);
 		setS(s);
 	}
+
+	void setTime() {
+		int x, y, z;
+		cout << "Enter hours, mins and seconds: ";
+		cin >> x >> y >> z;
+		setH(x);
+		setM(y);
+		setS(z);
 	}
+
 	void setH(int hour) {
-		this->hour = hour;
+		if (hour < 24)
+		{
+			this->hour = hour;
+		}
+		else
+		{
+			this->hour = 0;
+		}
 	}
 	int getH() {
 		return hour;
 	}
+
 	void setM(int min) {
-		this->min = min;
+		if (min < 60)
+		{
+			this->min = min;
+		}
+		else
+		{
+			this->min = 0;
+		}
 	}
 	int getM() {
 		return min;
 	}
+
 	void setS(int sec) {
-		this->sec = sec;
+		if (sec < 60)
+		{
+			this->sec = sec;
+		}
+		else
+		{
+			this->sec = 0;
+		}
 	}
 	int getS() {
 		return sec;
 	}
+
+	void add(int h, int m, int s) {
+		/////////////////////////////////////////////SECONDS///////////////////////////////
+		if (s >= 60)
+		{
+			int q = 0, w = 0;
+			do
+			{
+				s -= 60;
+				q++;
+				if (q >= 60)
+				{
+					q = 0;
+					w++;
+				}
+			} while (s >= 60);
+			if (s + sec == 60)
+			{
+				q++;
+				sec = 0;
+			}
+			sec += s;
+			if ((min + q) == 60)
+			{
+				min = 0;
+				hour++;
+			}
+			else if ((min + q) > 60)
+			{
+				min = min + q - 60;
+			}
+			else if (min + q < 60)
+			{
+				min += q;
+			}
+
+			if (w > 0)
+			{
+				if ((hour + w) >= 24)
+				{
+					hour = hour + w - 24;
+				}
+				else if((hour + w) < 24)
+				{
+					hour += w;
+				}
+			}
+		}
+		else if (s < 60)
+		{
+			if ((getS() + s) >= 60)
+			{
+				min++;
+				sec = sec + s - 60;
+			}
+			else if ((getS() + s) < 60)
+			{
+				sec += s;
+			}
+		}
+		///////////////////////////////////////////////////////////MINUTES////////////////////////////////////////
+		if (m >= 60)
+		{
+			int q = 0;
+			do
+			{
+				m -= 60;
+				q++;
+			} while (m >= 60);
+			if ((hour + q) >= 24)
+			{
+				hour = hour + q - 24;
+			}
+			else
+			{
+				hour += q;
+			}
+		}
+		if (m < 60)
+		{
+			if ((min + m) >= 60)
+			{
+				hour++;
+				min = min + m - 60;
+			}
+			else if ((min + m) < 60)
+			{
+				min += m;
+			}
+		}
+		//////////////////////////////////////////////////HOURS/////////////////////////////////////////////////
+		if (h >= 24)
+		{
+			int q = 0;
+			do
+			{
+				h -= 24;
+				q++;
+			} while (h >= 24);
+			if (hour + h >= 24)
+			{
+				hour = hour + h - 24;
+			}
+			else if (hour + h < 24)
+			{
+				hour = hour + h;
+			}
+		}
+		if (h < 24)
+		{
+			if (hour + h >= 24)
+			{
+				hour = hour + h - 24;
+			}
+			else if (hour + h < 24)
+			{
+				hour = hour + h;
+			}
+		}
+		//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	}
+
+	void difference(Time x, Time y) {
+		cout << x.getH() - y.getH() << ":" << x.getM() - y.getM() << ":" << x.getS() - y.getS() << endl;
+	}
+
+	void compare(Time x, Time y) {
+		cout << "Compare: " << abs(x.getH() - y.getH()) << ":" << abs(x.getM() - y.getM()) << ":" << abs(x.getS() - y.getS()) << endl;
+	}
+
+	void deg() {
+		double a = abs((hour * 30 + min / 2) - min * 6);
+		if (a >= 180)
+		{
+			a = 360 - a;
+		}
+		cout <<"Degree: " << a << endl;
+	}
+
 	void print() {
-		cout << "q";
+		cout << "Time is: ";
+		if (hour >= 10) cout << hour << ":";
+		else cout << "0" << hour << ":";
+		if (min >= 10) cout << min << ":";
+		else cout << "0" << min << ":";
+		if (sec >= 10) cout << sec << endl;
+		else cout << "0" << sec << endl;
 	}
 };
