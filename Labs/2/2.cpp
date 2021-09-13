87be3334af7eddf01112d7da42bf0de900cf01bc
Ilesha23 <ilyayakovlev0123@gmail.com>
2021-09-13 18:45:26 +0300

    finished lab 2a

--- Labs/2/2.cpp
+++ Labs/2/2.cpp
@@ -1,12 +1,49 @@
 ﻿#define N 3
 
+#include <conio.h>
+#include <string.h>
 #include <iostream>
+#include <iomanip>
 #include "Sport.h"
 using namespace std;
 
 int main()
 {
+	setlocale(LC_ALL, "");
 	Sport obj[N]{
-
+		Sport("qwert", "q", 123.5, 3),
+		Sport("qwert", "q", 123.5, 3),
+		Sport("zxcv", "z", 432.6, 1)
 	};
+
+	for (int i = 0; i < N; i++)
+	{
+		obj[i].print();
+	}
+
+	int a, b;
+	cout << endl << "Operator ==" << endl << "Choose objects: ";
+	cin >> a >> b;
+	obj[a] == obj[b];
+	_getch();
+	system("cls");
+	for (int i = 0; i < N; i++)
+	{
+		obj[i].print();
+	}
+
+	cout << endl << "Operator =" << endl << "Choose objects: ";
+	cin >> a >> b;
+	obj[a] = obj[b];
+	system("cls");
+	for (int i = 0; i < N; i++)
+	{
+		obj[i].print();
+	}
+
+	cout << endl << "Operator +" << endl << "Choose objects: ";
+	cin >> a >> b;
+	Sport q = obj[a] + obj[b];
+	cout << setw(10) << q.getName() << setw(7) << q.getCode() << setw(7) << q.getArea() << setw(7) << q.getPlace() << endl;
+
 }
--- Labs/2/Sport.h
+++ Labs/2/Sport.h
@@ -1,3 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+using namespace std;
+
 class Sport
 {
 private:
@@ -7,5 +12,62 @@ private:
 	int place;
 
 public:
+	Sport(string name, string code, double area, int place) {
+		setAll(name, code, area, place);
+	}
+	Sport() {
+		setAll("", "", 0, 0);
+	}
 
+	void setAll(string name, string code, double area, int place) {
+		this->name = name;
+		this->code = code;
+		this->area = area;
+		this->place = place;
+	}
+	string getName() {
+		return name;
+	}
+	string getCode() {
+		return code;
+	}
+	double getArea() {
+		return area;
+	}
+	int getPlace() {
+		return place;
+	}
+	
+	void print() {
+		cout << setw(10) << name << setw(7) << code << setw(7) << area << setw(7) << place << endl;
+	}
+
+	Sport& operator =(const Sport& ob) {
+		if (&ob != this)
+		{
+			name = ob.name;
+			code = ob.code;
+			area = ob.area;
+			place = ob.place;
+			return *this;
+		}
+	}
+
+	int operator ==(Sport& ob) {
+		if (name != ob.name) { cout << "Данные экземпляры класса не равны." << endl; }
+		else if (code != ob.code) { cout << "Данные экземпляры класса не равны." << endl; }
+		else if (area != ob.area) { cout << "Данные экземпляры класса не равны." << endl; }
+		else if (place != ob.place) { cout << "Данные экземпляры класса не равны." << endl; }
+		else cout << "Экземпляры класса равны." << endl;
+		return 0;
+	}
+
+	Sport operator +(Sport& ob) {
+		Sport q;
+		q.name = name + ob.name;
+		q.code = code + ob.code;
+		q.area = area + ob.area;
+		q.place = place +ob.place;
+		return q;
+	}
 };
