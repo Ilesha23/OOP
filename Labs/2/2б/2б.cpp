6695e01970da24e9165b5ebe37afd4223ae0a7db
Ilesha23 <ilyayakovlev0123@gmail.com>
2021-09-14 01:16:17 +0300

    finished lab 2б

---
+++ Labs/2/2б/2б.cpp
@@ -0,0 +1,65 @@
+﻿#define N 3
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <conio.h>
+#include <string.h>
+#include <iostream>
+#include <iomanip>
+#include "Sport.h"
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	char* n1 = new char[20]; strcpy(n1, "qwerty");
+	char* n2 = new char[20]; strcpy(n2, "asdfgh");
+	char* n3 = new char[20]; strcpy(n3, "zxcvbn");
+	Sport obj[N]{
+		Sport(n1, "q", 123.5, 3),
+		Sport(n2, "a", 123.5, 3),
+		Sport(n3, "z", 432.6, 1)
+	};
+
+	for (int i = 0; i < N; i++)
+	{
+		obj[i].print();
+	}
+
+	int a, b;
+	cout << endl << "Operator ==" << endl << "Choose objects: ";
+	cin >> a >> b;
+	obj[a] == obj[b];
+	_getch();
+	system("cls");
+	for (int i = 0; i < N; i++)
+	{
+		obj[i].print();
+	}
+
+	/*cout << endl << "Operator =" << endl << "Choose objects: ";
+	cin >> a >> b;
+	obj[a] = obj[b];
+	system("cls");
+	for (int i = 0; i < N; i++)
+	{
+		obj[i].print();
+	}*/
+
+	cout << endl << "Operator +" << endl << "Choose objects: ";
+	cin >> a >> b;
+	Sport q = obj[a] + obj[b];
+	cout << setw(10) << q.getName() << setw(7) << q.getCode() << setw(7) << q.getArea() << setw(7) << q.getPlace() << endl;
+	_getch();
+
+	system("cls");
+	for (int i = 0; i < N; i++)
+	{
+		obj[i].print();
+	}
+	cout << endl << "Operator >>" << endl << "Enter: ";
+	Sport w;
+	cin >> w;
+
+	cout << endl << "Operator <<" << endl;
+	cout << w;
+}
---
+++ Labs/2/2б/Sport.h
@@ -0,0 +1,118 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+using namespace std;
+
+class Sport
+{
+private:
+	char* name;
+	string code;
+	double area;
+	int place;
+	friend Sport operator+(const Sport& ob1, const Sport& ob2);
+	friend int operator==(const Sport& ob1, const Sport& ob2);
+	friend ostream& operator<<(ostream& stream, Sport& ob1);
+	friend istream& operator>>(istream& stream, Sport& ob1);
+
+public:
+	Sport(char* a, string code, double area, int place) {
+		setAll(a, code, area, place);
+	}
+	Sport() {
+		name = new char[10];
+		strcpy(name, "poiuyt");
+		this->code = "g";
+		this->area = 123.7;
+		this->place = 5;
+	}
+
+	void setAll(char* a, string code, double area, int place) {
+		name = new char[strlen(a) + 1];
+		strcpy(name, a);
+		this->code = code;
+		this->area = area;
+		this->place = place;
+	}
+	void setName(char* a) {
+		strcpy(name, a);
+	}
+	char* getName() {
+		return name;
+	}
+	void setCode(string code) {
+		this->code = code;
+	}
+	string getCode() {
+		return code;
+	}
+	void setArea(double area) {
+		this->area = area;
+	}
+	double getArea() {
+		return area;
+	}
+	void setPlace(int place) {
+		this->place = place;
+	}
+	int getPlace() {
+		return place;
+	}
+
+	void print() {
+		cout << setw(10) << name << setw(7) << code << setw(7) << area << setw(7) << place << endl;
+	}
+
+	/*Sport& operator =(const Sport& ob) {
+		delete[] name;
+		name = new char[strlen(ob.name) + 2];
+		if (!name) {
+			cout << "Ошибка! Память не выделена.";
+			exit(1);
+		}
+		strcpy(name, ob.name);
+		code = ob.code;
+		area = ob.area;
+		place = ob.place;
+		return *this;
+	}*/
+};
+
+Sport operator+(const Sport& ob1, const Sport& ob2)
+{
+	Sport q;
+	delete[] q.name;
+	q.name = new char[strlen(ob1.name) + strlen(ob2.name) + 2];
+	strcpy(q.name, ob1.name);
+	strcat(q.name, ob1.name);
+	strcpy(q.name, ob2.name);
+	strcat(q.name, ob2.name);
+	q.setCode(ob1.code + ob2.code);
+	q.setArea(ob1.area + ob2.area);
+	q.setPlace(ob1.place + ob2.place);
+	return q;
+}
+inline int operator==(const Sport& ob1, const Sport& ob2)
+{
+	if (ob1.name != ob2.name) { cout << "Данные экземпляры класса не равны." << endl; }
+	else if (ob1.code != ob2.code) { cout << "Данные экземпляры класса не равны." << endl; }
+	else if (ob1.area != ob2.area) { cout << "Данные экземпляры класса не равны." << endl; }
+	else if (ob1.place != ob2.place) { cout << "Данные экземпляры класса не равны." << endl; }
+	else cout << "Экземпляры класса равны." << endl;
+	return 0;
+}
+
+inline ostream& operator<<(ostream& stream, Sport& ob1)
+{
+	stream << setw(10) << ob1.name << setw(7) << ob1.code << setw(7) << ob1.area << setw(7) << ob1.place << endl;
+	return stream;
+}
+
+inline istream& operator>>(istream& stream, Sport& ob1)
+{
+	cout << "Name: "; stream >> ob1.name;
+	cout << "Code: "; stream >> ob1.code;
+	cout << "Area: "; stream >> ob1.area;
+	cout << "Place: "; stream >> ob1.place;
+	return stream;
+}
