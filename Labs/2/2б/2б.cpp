e2ac4e536d6cc670b6dd44bead91c116917cf90f
Ilesha23 <ilyayakovlev0123@gmail.com>
2021-09-25 20:18:44 +0300

    lab2б

--- Labs/2/2б/2б.cpp
+++ Labs/2/2б/2б.cpp
@@ -11,11 +11,11 @@ using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "");
-	char* n1 = new char[20]; strcpy(n1, "qwerty");
-	char* n2 = new char[20]; strcpy(n2, "asdfgh");
-	char* n3 = new char[20]; strcpy(n3, "zxcvbn");
+	char* n1 = new char[20]; strcpy(n1, "asdfghop");
+	char* n2 = new char[20]; strcpy(n2, "asdfghop");
+	char* n3 = new char[20]; strcpy(n3, "zxcvbnopop");
 	Sport obj[N]{
-		Sport(n1, "q", 123.5, 3),
+		Sport(n1, "a", 123.5, 3),
 		Sport(n2, "a", 123.5, 3),
 		Sport(n3, "z", 432.6, 1)
 	};
@@ -56,10 +56,39 @@ int main()
 	{
 		obj[i].print();
 	}
-	cout << endl << "Operator >>" << endl << "Enter: ";
+	cout << endl << endl << "Operator >>" << endl << "Enter: ";
 	Sport w;
 	cin >> w;
 
 	cout << endl << "Operator <<" << endl;
 	cout << w;
+	_getch();
+
+	system("cls");
+	for (int i = 0; i < N; i++)
+	{
+		obj[i].print();
+	}
+	cout << endl << "Operator []" << endl << "Enter: ";
+	int r;
+	cin >> r;
+	cout << obj[r][r];
+	_getch();
+
+	system("cls");
+	for (int i = 0; i < N; i++)
+	{
+		obj[i].print();
+	}
+	cout << endl << endl << "Operator ()" << endl << "Enter: ";
+	char* an = new char[20];
+	string cc;
+	double aa;
+	int pp;
+	cout << endl << "Name: "; cin >> an;
+	cout << "Code: "; cin >> cc;
+	cout << "Area: "; cin >> aa;
+	cout << "Place: "; cin >> pp;
+	obj[1](an, cc, aa, pp);
+	cout << obj[1];
 }
