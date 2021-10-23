4839a5361f9b3bf4e82bb9546a0d7bef1e34b521
Ilesha23 <ilyayakovlev0123@gmail.com>
2021-10-23 13:52:08 +0300

    lab5 finished

--- Labs/5/5.cpp
+++ Labs/5/5.cpp
@@ -1,11 +1,20 @@
 ﻿#include <iostream>
 #include <iomanip>
-#include <string>
+#include <bitset>
+#include <vector>
 using namespace std;
 
 int main()
 {
-	int a = -10;
-	long fl = cout.flags();
-	cout << fl;
+	vector<int> a;
+	srand(time(NULL));
+	for (int i = 0; i < 256; i++)
+	{
+		a.push_back(rand() % 1000);
+	}
+	cout << setw(10) << "DEC" << setw(10) << "HEX" << setw(10) << "OCT" << setw(13) << "BIN" << endl << endl;
+	for (int i = 0; i < a.size(); i++)
+	{
+		cout << setw(10) << a[i] << setw(10) << hex << a[i] << setw(10) << oct << a[i] << setw(15) << bitset<8>(a[i]) << endl;
+	}
 }
