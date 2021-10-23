60168068e0bc0e8b7b1497e96e52c5603b787784
Ilesha23 <ilyayakovlev0123@gmail.com>
2021-10-23 17:07:49 +0300

    8a finished

---
+++ Labs/8/8a/8a.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <fstream>
+#include "number.h"
+using namespace std;
+
+int main()
+{
+	number <double> a;
+	number<double> b;
+	cin >> a;
+	cin >> b;
+	number<double> c;
+	c = a / b;
+	cout << c << endl;	
+	c = a * b;
+	cout << c << endl;
+	c = a + b;
+	cout << c << endl;
+	c = a - b;
+	cout << c << endl;
+}
---
+++ Labs/8/8a/number.h
@@ -0,0 +1,59 @@
+#include <fstream>
+using namespace std;
+
+template<class T>
+class number
+{
+public:
+	T num;
+	number() {
+		num = 0;
+	}
+	number(T a) {
+		num = a;
+	}
+
+	template<class T>
+	friend ostream& operator << (ostream& , number<T>& );
+	template<class T>
+	friend istream& operator >> (istream&, number<T>&);
+
+};
+
+template <class T>
+ostream& operator<<(ostream& stream, number<T>& a)
+{
+	stream << a.num;
+	return stream;
+}
+template <class T>
+istream& operator>>(istream& stream, number<T>& a)
+{
+	stream >> a.num;
+	return stream;
+}
+
+template<class T>
+number<T> operator / (number<T>& a, number<T>& b)
+{
+	T c = a.num / b.num;
+	return c;
+}
+template<class T>
+number<T> operator * (number<T>& a, number<T>& b)
+{
+	T c = a.num * b.num;
+	return c;
+}
+template<class T>
+number<T> operator + (number<T>& a, number<T>& b)
+{
+	T c = a.num + b.num;
+	return c;
+}
+template<class T>
+number<T> operator - (number<T>& a, number<T>& b)
+{
+	T c = a.num - b.num;
+	return c;
+}
