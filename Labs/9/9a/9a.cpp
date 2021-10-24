4c10d37af79f327a7b1c78578823571fddb73a55
Ilesha23 <ilyayakovlev0123@gmail.com>
2021-10-25 00:54:46 +0300

    9a improved

--- Labs/9/9a/9a.cpp
+++ Labs/9/9a/9a.cpp
@@ -8,13 +8,53 @@ int main()
 	vector<Vars> v1;
 	vector<Vars> v2;
 
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < 5; i++)
 	{
 		v1.push_back(Vars{ i, i });
 		v2.push_back(Vars{ i, i});
 	}
 
-	for (int i = 0; i < v1.size(); i++)
+	for (int i = 0; i < 5; i++)
+	{
+		if (v1[i] == v2[i])
+			cout << i << "   " << v1[i].getSum() << " == " << v2[i].getSum() << endl;
+		else if (v1[i] > v2[i])
+			cout << i << "   " << v1[i].getSum() << " > " << v2[i].getSum() << endl;
+		else if (v1[i] < v2[i])
+			cout << i << "   " << v1[i].getSum() << " < " << v2[i].getSum() << endl;
+	}
+	cout << endl;
+
+	v1.clear();
+	v2.clear();
+
+	for (int i = 0; i < 5; i++)
+	{
+		v1.push_back(Vars{ i+1, i*2 });
+		v2.push_back(Vars{ i, i });
+	}
+
+	for (int i = 0; i < 5; i++)
+	{
+		if (v1[i] == v2[i])
+			cout << i << "   " << v1[i].getSum() << " == " << v2[i].getSum() << endl;
+		else if (v1[i] > v2[i])
+			cout << i << "   " << v1[i].getSum() << " > " << v2[i].getSum() << endl;
+		else if (v1[i] < v2[i])
+			cout << i << "   " << v1[i].getSum() << " < " << v2[i].getSum() << endl;
+	}
+	cout << endl;
+
+	v1.clear();
+	v2.clear();
+
+	for (int i = 0; i < 5; i++)
+	{
+		v1.push_back(Vars{ i, i });
+		v2.push_back(Vars{ i+1, i*2 });
+	}
+
+	for (int i = 0; i < 5; i++)
 	{
 		if (v1[i] == v2[i])
 			cout << i << "   " << v1[i].getSum() << " == " << v2[i].getSum() << endl;
