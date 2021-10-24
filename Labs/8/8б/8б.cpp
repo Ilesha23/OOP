e4376a37b48f195c5df5485fef6d5cb8d8261b90
Ilesha23 <ilyayakovlev0123@gmail.com>
2021-10-24 23:33:01 +0300

    8б improved

--- Labs/8/8б/8б.cpp
+++ Labs/8/8б/8б.cpp
@@ -8,7 +8,8 @@ int main()
 	int q = 1;
 	for (size_t i = 0; i < a.size(); i++)
 	{
-		a.fillElem(q++, i);
+		a.fillElem(q, i);
+		q++;
 	}
 	a.print();
 	cout << endl << "Sum of elemets is: " << a.sum() << endl << "Average is: " << a.avg() << endl << "Max is: " << a[1] << endl;
